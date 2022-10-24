10b4af5f8ad93dc122829a5097c6764084975f3c
Darryl Huffman <dyudabomb@gmail.com>
2022-10-24 15:08:43 -0400

    Added webpack to build project

---
+++ src/index.js
@@ -0,0 +1,119 @@
+import MetaRIBBITConnect from 'ribbit-connect-meta';
+import { stylesheet } from './styles'
+
+export default class RIBBITConnect {
+    initialized = false;
+    parent = null;
+
+    open = false;
+
+
+    popupElement = null;
+    inlineElement = null;
+
+    messageCallbacks = []
+    
+    constructor({
+        target,
+        token,
+        language,
+        inline = false,
+        settings = {},
+        className,
+        open = false
+    }){
+        this.targetId = target;
+        this.token = token;
+        this.language = language;
+        this.inline = inline;
+        this.settings = settings;
+        this.className = className;
+        this.isOpen = open;
+
+        this.curtainColor = settings.curtainColor;
+        this.curtainAllowClose = settings.curtainAllowClose || true;
+
+        this.RIBBITConnectContext = new MetaRIBBITConnect({ token, settings, inline, language });
+        this.init()
+    }
+
+    init = () => {
+        if(this.initialized) return;
+        this.parent = document.getElementById(this.targetId)
+
+        if(this.inline) this.initInline();
+        else this.initPopup();
+
+        this.initStyles();
+
+        this.RIBBITConnectContext.CONNECTEvents.map(eventName => {
+            const capitalizedEventName = eventName.charAt(0).toUpperCase() + eventName.slice(1);
+            
+            this['on' + capitalizedEventName] = (callback) => {
+                this.RIBBITConnectContext.on(eventName, callback)
+            }
+        })
+        
+        this.RIBBITConnectContext.onMessage((functionName, event) => {
+            this.messageCallbacks.map(x => {
+                if(functionName != 'CONNECTEvent' && x.getAllMessages == false) return;
+                x.callback(functionName, event)
+            })
+        })
+    }
+
+    initPopup = () => {
+        this.popupElement = document.createElement('div')
+
+        const curtain = document.createElement('div')
+        curtain.className = 'RIBBIT-popup-curtain'
+        if(this.curtainColor) curtain.style.backgroundColor = this.curtainColor;
+        this.popupElement.appendChild(curtain)
+        this.element = this.popupElement;
+
+        this.popupElement.appendChild(this.RIBBITConnectContext.iFrame)
+        this.popupElement.addEventListener('click', this.closePopup)
+        this.parent.appendChild(this.popupElement)
+        this.updateClassName();
+    }
+
+    initStyles = () => {
+        var style = document.createElement('style');
+        style.innerHTML = stylesheet;
+        document.head.appendChild(style);
+    }
+
+    initInline = () => {
+        this.inlineElement = document.createElement('div')
+        this.inlineElement.appendChild(this.RIBBITConnectContext.iFrame)
+        this.parent.appendChild(this.inlineElement)
+    }
+
+    updateClassName = () => {
+        this.element.className = `RIBBIT-popup ${this.className ? ' ' + this.className + ' ' : ''} ${this.isOpen ? ' RIBBIT-popup-open' : ''} ${this.inline ? ' RIBBIT-popup-inline' : ' RIBBIT-popup-popup'}`
+    }
+
+    onMessage = (callback, getAllMessages = false) => {
+        messageCallbacks.push({ callback, getAllMessages })
+    }
+
+    open = () => {
+        this.isOpen = true;
+        this.updateClassName();
+    }
+
+    close = () => {
+        this.isOpen = false;
+        this.updateClassName();
+    }
+
+    closePopup = () => {
+        if(!this.curtainAllowClose) return;
+        this.RIBBITConnectContext.sendMessage('exit', null)
+    }
+
+}
+
+((window, document) => {
+    window.RIBBITConnect = RIBBITConnect;
+})(window, document)
---
+++ src/styles.js
@@ -0,0 +1,58 @@
+const stylesheet = `
+.RIBBIT-iFrame {
+    margin: auto;
+    border-radius: 10px;
+    border: 0px transparent;
+}
+.RIBBIT-popup {
+    position: fixed;
+    top: 0px;
+    left: 0px;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    z-index: 10;
+}
+.RIBBIT-popup.RIBBIT-popup-popup {
+    pointer-events: none;
+    opacity: 0;
+    transition: opacity 300ms;
+}
+.RIBBIT-popup.RIBBIT-popup-popup .RIBBIT-iFrame {
+    transform: translateY(50vh) scale(0.9);
+    transition: transform 500ms;
+}
+.RIBBIT-popup.RIBBIT-popup-popup.RIBBIT-popup-open {
+    pointer-events: all;
+    opacity: 1;
+}
+.RIBBIT-popup.RIBBIT-popup-popup.RIBBIT-popup-open .RIBBIT-iFrame {
+    transform: translateY(0vh) scale(1);
+}
+.RIBBIT-popup .RIBBIT-iFrame {
+    position: relative;
+    z-index: 2;
+}
+.RIBBIT-popup .RIBBIT-iFrame.mobile {
+    width: 100% !important;
+    height: 98% !important;
+    bottom: 0px;
+    position: absolute;
+    border-top-left-radius: 10px;
+    border-top-right-radius: 10px;
+    border-bottom-left-radius: 0;
+    border-bottom-right-radius: 0;
+    box-shadow: 0 -2px 10px #000;
+}
+.RIBBIT-popup .RIBBIT-popup-curtain {
+    position: absolute;
+    z-index: 1;
+    top: 0px;
+    left: 0px;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+}
+`
+
+export { stylesheet }
